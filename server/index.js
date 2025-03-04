13259bc2c1ac318d83f1ce0a7f3783309f688a55
Matt <matthewpimentel@hotmail.com>
2025-03-03 20:42:26 -0500

    added basic server

---
+++ server/index.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const http = require('http');
+const socketIo = require('socket.io');
+const cors = require('cors');
+require('dotenv').config();
+
+const app = express();
+const server = http.createServer(app);
+const io = socketIo(server, { cors: { origin: '*' } });
+
+app.use(cors());
+app.use(express.json());
+
+const PORT = process.env.PORT || 5000;
+server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
